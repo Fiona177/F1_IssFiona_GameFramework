5ae95a3888648227112f3a1e0fc76a7b43c60d52
Fiona177 <32631776+Fiona177@users.noreply.github.com>
2019-05-11 16:27:57 +0200

    Update progprincipal.pas

--- progprincipal.pas
+++ progprincipal.pas
@@ -8,42 +8,43 @@ var bal1,bal2,bal3:ball;
 BEGIN
 	clrscr;
 	nb:=10;
-	//Utilisation des procédures Set pour définir la position de mon vecteur 1
+	//Utilisation des procédures Set pour définir la position de la balle 1
 	bal1.obj.position.posX:=5;
 	setVectposX(bal1.obj.position,2);
 	setVectposY(bal1.obj.position,nb);
 
 	writeln(bal1.obj.position.posX);
 	writeln(bal1.obj.position.posY);
-	//Utilisation des procédures Set pour définir la position de mon vecteur 2
+	//Utilisation des procédures Set pour définir la position de la balle 2
 	setVectposX(bal2.obj.position,5);
 	setVectposY(bal2.obj.position,7);
 
 	writeln(bal2.obj.position.posX);
 	writeln(bal2.obj.position.posY);
-	//J'affecte au vecteur 3 l'addition des vecteurs 1 et 2
+	//J'affecte a la balle 3 l'addition des balles 1 et 2
 	bal3.obj.position:=addiVect(bal1.obj.position,bal2.obj.position);
 
 	writeln(bal3.obj.position.posX);
 	writeln(bal3.obj.position.posY);
-	//J'affecte au vecteur 3 la soustraction des vecteurs 1 et 2
+	//J'affecte a la balle 3 la soustraction des balles 1 et 2
 	bal3.obj.position:=sousVect(bal1.obj.position,bal2.obj.position);
 
 	writeln(bal3.obj.position.posX);
 	writeln(bal3.obj.position.posY);
-	//J'affecte au vecteur 3 le produit par un scalaire du vecteur 1
+	//J'affecte a la balle 3 le produit par un scalaire de la balle 1
 	bal3.obj.position:=prodparscaVect(bal1.obj.position,5);
 
 	writeln(bal3.obj.position.posX);
 	writeln(bal3.obj.position.posY);
-	//J'affecte à nb le produit scalaire des vecteurs 1 et 2
-	nb:=proscaVect(bal1.obj.position,bal2.obj.position);
+	//J'affecte à nb le produit scalaire des balles 1 et 2
+	bal3.obj.position:=proscaVect(bal1.obj.position,bal2.obj.position);
 
 	writeln(nb);
-	//J'affecte à nb le produit vectoriel des vecteurs 1 et 2
-	nb:=provecVect(bal1.obj.position,bal2.obj.position);
+	//J'affecte a la balle 3 le produit vectoriel des balles 1 et 2
+	bal3.obj.position:=provecVect(bal1.obj.position,bal2.obj.position);
 
-	writeln(nb);
+	writeln(bal3.obj.position.posX);
+	writeln(bal3.obj.position.posY);
 
 	readln;
 END.
